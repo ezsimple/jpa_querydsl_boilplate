e697aa201ee7fbc2e5661c563d3f5d6319313fe9
mhlee <mhlee@ezfarm.co.kr>
2021-10-04 01:46:31 +0900

    .

--- src/main/java/com/example/demo2/api/hello/controller/HelloController.java
+++ src/main/java/com/example/demo2/api/hello/controller/HelloController.java
@@ -3,10 +3,12 @@ package com.example.demo2.api.hello.controller;
 import com.example.demo2.api.entity.User;
 import com.example.demo2.api.hello.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,9 +22,9 @@ public class HelloController {
         return "Hello World9";
     }
 
-    @GetMapping("/user/{id}")
-    public ResponseEntity<User> findById(@PathVariable("id") String id) {
+    @GetMapping(value = "/user/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public User findById(@PathVariable("id") String id) {
         User user = helloService.findById(id);
-        return ResponseEntity.ok(user);
+        return user;
     }
 }
