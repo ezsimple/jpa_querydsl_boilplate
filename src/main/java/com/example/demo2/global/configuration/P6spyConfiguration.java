6c7ca8cad55a53089946b7ebc35bab93b42f8365
mhlee <mkeasy@gmail.com>
2021-12-08 14:34:03 +0900

    .

---
+++ src/main/java/com/example/demo2/global/configuration/P6spyConfiguration.java
@@ -0,0 +1,14 @@
+package com.example.demo2.global.configuration;
+
+import com.p6spy.engine.spy.P6SpyOptions;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+public class P6spyConfiguration {
+    @PostConstruct
+    public void setLogMessageFormat() {
+        P6SpyOptions.getActiveInstance().setLogMessageFormat(P6spyPrettySqlFormatter.class.getName());
+    }
+}
